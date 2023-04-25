1fdfe751bd84081fdf017872623d0d5672b304a7
ChiduAnush <chiduanush1@gmail.com>
2023-04-25 22:07:04 +0530

    added the local storage functionality for name and focus

--- main.js
+++ main.js
@@ -45,7 +45,62 @@ function setgreet() {
 
 }
 
+//get name to local storage
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[enter name]'
+
+    } else {
+        name.textContent = localStorage.getItem('name')
+    }
+}
+
+//to set name to local storage
+function setName(e) {
+    if (e.type === 'keypress') {                                //for pressing enter
+        //make sure 'enter' is pressed
+        if (e.which == 13 || e.keycode == 13) {                 //13 is the unique keycode for 'enter' key
+            localStorage.setItem('name', e.target.innerText)
+            name.blur()
+        }
+    } else {                                                    //for blur
+        localStorage.setItem('name', e.target.innerText)
+    }
+}
+
+
+
+//get focus lines to local storage
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = 'hmm? this space seems empty'
+    } else {
+        focus.textContent = localStorage.getItem('focus')
+    }
+}
+
+//to set focus to local storage
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        if (e.keycode == 13 || e.which == 13) {
+            localStorage.setItem('focus', e.target.innerText)
+            focus.blur()
+        }
+
+    } else {
+        localStorage.setItem('focus', e.target.innerText)
+    }
+}
+
+name.addEventListener('keypress', setName)
+name.addEventListener('blur', setName)
+
+focus.addEventListener('keypress', setFocus)
+focus.addEventListener('blur', setFocus)
+
 
 //run
 showTime()
 setgreet()
+getName()
+getFocus()
